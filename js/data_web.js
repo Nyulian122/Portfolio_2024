600ca1d372818056b83514b98575ef528c46cc5c
Nyulian122 <103065477+Nyulian122@users.noreply.github.com>
2024-03-15 10:37:02 -0600

    Add files via upload

--- js/data_web.js
+++ js/data_web.js
@@ -1,35 +1,35 @@
 let lista_webs = [
     {
         nombre: "EVUPAY",
-        img: "",
+        img: "https://github.com/Nyulian122/imagenes_prop/blob/main/Screenshot%20from%202024-03-15%2008-50-25.png?raw=true",
         link_repo: "https://github.com/Nyulian122/EVUPAY-COMPLETO",
         github_page: "https://nyulian122.github.io/EVUPAY-COMPLETO/",
         descripcion: "Esta fue una de las tareas mas feas que hecho en el ano de carrera"
     },
     {
         nombre: "Collage de fotos",
-        img: "",
+        img: "https://github.com/Nyulian122/imagenes_prop/blob/main/Screenshot%20from%202024-03-15%2008-52-00.png?raw=true",
         link_repo: "https://github.com/Nyulian122/COSITACHINA",
         github_page: "https://nyulian122.github.io/COSITACHINA/",
         descripcion: "Se realizo un collage de fotos con display flex"
     },
     {
         nombre: "Examen de retrasadas",
-        img: "Header del parqueo",
+        img: "https://github.com/Nyulian122/imagenes_prop/blob/main/Screenshot%20from%202024-03-15%2008-53-39.png?raw=true",
         link_repo: "https://github.com/Nyulian122/HEADER",
         github_page: "https://nyulian122.github.io/HEADER/",
         descripcion: "Este fue realizado en retrasadas, fue una prueba de grid"
     },
     {
         nombre: "Boton en JS",
-        img: "",
+        img: "https://github.com/Nyulian122/imagenes_prop/blob/main/Screenshot%20from%202024-03-15%2008-55-12.png?raw=true",
         link_repo: "https://github.com/Nyulian122/BOTON",
         github_page: "https://nyulian122.github.io/BOTON/",
         descripcion: "Fue una de las primeras actividades con un action listener en js"
     },
     {
         nombre: "Evaluacion II periodo 2023",
-        img: "",
+        img: "https://github.com/Nyulian122/imagenes_prop/blob/main/Screenshot%20from%202024-03-15%2009-57-25.png?raw=true",
         link_repo: "https://github.com/Nyulian122/EVALUACION",
         github_page: "https://nyulian122.github.io/EVALUACION/",
         descripcion: "FUe una de las 2das evualuaciones que realize el ano pasado"
